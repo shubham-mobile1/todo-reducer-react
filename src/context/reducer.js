e122fb1fae0874a48458ed2aa7db77edb0ee942a
shubham-mobile1 <shubhambundela.mobilefirst@gmail.com>
2022-08-03 21:50:45 +0530

    Added updated code

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,13 +1,12 @@
-import { ADD_TODO, REMOVE_TODO } from "./action.types";
+import {ADD_TODO, REMOVE_TODO} from "./actions.types";
 
-export default (state, action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return [...state, action.payload];
-    case REMOVE_TODO:
-      return state.filter((todo) => todo.id !== action.payload);
-
-    default:
-      return state;
-  }
+export default (state, actions) => {
+    switch(actions.type){
+        case ADD_TODO:
+            return [...state, actions.payload];
+        case REMOVE_TODO:
+            return state.filter(todo => todo.id !== actions.payload);
+        default:
+            return state;
+    }
 };
